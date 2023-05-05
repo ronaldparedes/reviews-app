8ff235d2c2c3ec0bd4087d6a4d86ae1ac2b14101
Ronald Paredes <ronald825@gmail.com>
2023-05-04 21:45:03 -0400

    set fetch return to json

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var http = require("http");
+import fetch from "node-fetch";
+import http from "http";
 http
   .createServer(function (req, res) {
     // console.log(`Just got a request at ${req.url}!`)
     // res.write('Yo!');
     // res.end();
-    import fetch from "node-fetch";
 
     const YELP_API_KEY =
       "w8uQv046531_c25osdbeZm3kkvK3G5I5DmK9bmIeeuJuOatLqVrV9KVWss7EaFvbW3cYkcricx9ItFTWnWH7LfC-Xvs4F-ldIwz4x_Re9eaJrXJkxIVRl8ryAecRX3Yx";
@@ -21,7 +21,8 @@ http
     })
       .then((response) => response.json())
       .then((result) => {
-        console.log(result.reviews);
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(result.reviews));
       })
       .catch((error) => {
         console.log("error", error);
