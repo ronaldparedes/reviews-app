acf70cfbefd3d4801c89cb9c07029167701921ba
Ronald Paredes <ronald825@gmail.com>
2023-05-21 20:53:27 -0400

    added google reviews

--- index.js
+++ index.js
@@ -12,10 +12,15 @@ app.get("/yelp-review", async (req, res) => {
   const result = await yelpReview(req.query.business_id);
   res.json(result);
 });
+app.get("/google-review", async (req, res) => {
+  const result = await googleReview();
+  res.json(result);
+});
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const yelpReview = async (businessId) => {
+const optimalBusinessId = "o9ds9z_6W2bFhAr0yA6HdA";
+const yelpReview = async (businessId = optimalBusinessId) => {
   const yelpAPI_URL = `https://api.yelp.com/v3/businesses/${businessId}/reviews`;
   return await fetch(yelpAPI_URL, {
     method: "GET",
@@ -26,6 +31,17 @@ const yelpReview = async (businessId) => {
   })
     .then((response) => response.json())
     .then((res) => res.reviews)
+    .catch((error) => {
+      console.log("error", error);
+    });
+};
+
+const googleReview = async () => {
+  const place_id = "ChIJEaHl4dwttokRnZca2ELCk9w";
+  const googleAPI_URL = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${place_id}&fields=review&key=${process.env.GOOGLE_API_KEY}`;
+  return await fetch(googleAPI_URL)
+    .then((response) => response.json())
+    .then((res) => res.result.reviews)
     .catch((error) => {
       console.log("error", error);
     });
